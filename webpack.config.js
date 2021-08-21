467b72cff13c2884471ddedc4b1043af8a85a78d
magcho <mail@magcho.com>
2021-08-21 14:40:19 +0900

    Update Fix dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "production",
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "umd"),
+    path: path.resolve(__dirname, "dist", "umd"),
     filename: "my-typescript-package.js",
     library: "MyTsPackage",
     libraryTarget: "umd"
